a8c5fc27446cc5fe8dd273895e6ce30ae7dd4a4b
Casper <42980888+BitesizedLion@users.noreply.github.com>
2023-12-31 03:07:27 +0100

    lol

--- src/main/kotlin/app/revanced/patches/imgur/ad/fingerprints/GetShowFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/imgur/ad/fingerprints/GetShowFingerprint.kt
@@ -1,4 +1,4 @@
-package app.revanced.patches.irplus.ad.fingerprints
+package app.revanced.patches.imgur.ad.fingerprints
 
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
