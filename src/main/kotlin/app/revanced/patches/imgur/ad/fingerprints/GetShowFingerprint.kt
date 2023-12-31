3e06d7c67606ad1c3c7a135a2a1e87fce2315d6f
Casper <42980888+BitesizedLion@users.noreply.github.com>
2023-12-31 03:17:43 +0100

    Update GetShowFingerprint.ktxxxxxx

--- src/main/kotlin/app/revanced/patches/imgur/ad/fingerprints/GetShowFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/imgur/ad/fingerprints/GetShowFingerprint.kt
@@ -1,13 +1,9 @@
 package app.revanced.patches.imgur.ad.fingerprints
 
-import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
-import com.android.tools.smali.dexlib2.AccessFlags
 
-object GetShowFingerprint : MethodFingerprint(
-    returnType = "Z", // boolean return type
-    access = AccessFlags.PUBLIC, // public method
-    parameters = emptyList(), // no parameters
-    opcodes = listOf(Opcode.IRETURN), // return instruction for int (boolean is represented as int in bytecode)
-    customFingerprint = { it.name == "getShow" && it.definingClass == "Lcom/imgur/mobile/common/AdsEligibility;" }
+internal object PrimaryAdsFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef, _ ->
+        methodDef.definingClass.endsWith("AdsEligibility;") && methodDef.name == "getShow"
+    }
 )
