65901e4e7ea2c5a1a2f612823e99f435d57f734a
Casper <42980888+BitesizedLion@users.noreply.github.com>
2023-12-31 03:22:09 +0100

    lol final??

--- src/main/kotlin/app/revanced/patches/imgur/ad/fingerprints/GetShowFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/imgur/ad/fingerprints/GetShowFingerprint.kt
@@ -2,7 +2,7 @@ package app.revanced.patches.imgur.ad.fingerprints
 
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
-internal object PrimaryAdsFingerprint : MethodFingerprint(
+internal object GetShowFingerprint : MethodFingerprint(
     customFingerprint = { methodDef, _ ->
         methodDef.definingClass.endsWith("AdsEligibility;") && methodDef.name == "getShow"
     }
