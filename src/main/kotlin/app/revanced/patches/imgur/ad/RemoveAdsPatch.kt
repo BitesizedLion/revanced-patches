4a508e42c27f66f3b58dd5abc843db058d859c41
Casper <42980888+BitesizedLion@users.noreply.github.com>
2023-12-31 02:45:18 +0100

    Create RemoveAdsPatch.kt

---
+++ src/main/kotlin/app/revanced/patches/imgur/ad/RemoveAdsPatch.kt
@@ -0,0 +1,30 @@
+package app.revanced.patches.imgur.ad
+
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.imgur.ad.fingerprints.IrplusAdsFingerprint
+
+@Patch(
+    name = "Always False for getShow",
+    description = "Patch to always return false for getShow",
+    compatiblePackages = [CompatiblePackage("com.imgur.mobile")]
+)
+object AlwaysFalseForGetShowPatch : BytecodePatch(
+    setOf(GetShowFingerprint)
+) {
+    override fun execute(context: BytecodeContext) {
+        val result = GetShowFingerprint.result
+            ?: throw PatchException("GetShowFingerprint not found")
+
+        result.mutableMethod.replaceInstructions(
+            0,
+            """
+                const/4 v0, 0x0 // Always return false
+                return v0
+            """
+        )
+    }
+}
