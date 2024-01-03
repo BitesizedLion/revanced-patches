a168a25f9daa5e948eae81f55e032e0b458765bd
Casper <42980888+BitesizedLion@users.noreply.github.com>
2024-01-03 03:09:54 +0100

    fix(asdf): i cba

--- src/main/kotlin/app/revanced/patches/omni/ad/RemoveAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/omni/ad/RemoveAdsPatch.kt
@@ -1,17 +1,29 @@
 package app.revanced.patches.omni.ad
 
-import app.revanced.util.exception
 import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.removeInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
-import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.options.PatchOption.PatchExtensions.stringPatchOption
+import app.revanced.patcher.patch.PatchException
 import app.revanced.patches.omni.ad.fingerprints.LinkFingerprint
-import com.android.tools.smali.dexlib2.iface.Method
+import app.revanced.patches.twitter.misc.links.fingerprints.LinkBuilderFingerprint
+import app.revanced.patches.twitter.misc.links.fingerprints.LinkResourceGetterFingerprint
+import app.revanced.util.exception
+import app.revanced.util.getReference
+import app.revanced.util.indexOfFirstInstruction
+import com.android.tools.smali.dexlib2.builder.BuilderInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.Instruction
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.Method
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
+import com.android.tools.smali.dexlib2.Opcode
 
 @Patch(
     name = "Disable ads",
