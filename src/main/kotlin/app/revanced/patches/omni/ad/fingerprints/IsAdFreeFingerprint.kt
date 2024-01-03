d48486794288b7bb17fbfc09f2b2bb2bed40d1bb
Casper <42980888+BitesizedLion@users.noreply.github.com>
2024-01-03 03:04:29 +0100

    Delete src/main/kotlin/app/revanced/patches/omni/ad/fingerprints/IsAdFreeFingerprint.kt

--- src/main/kotlin/app/revanced/patches/omni/ad/fingerprints/IsAdFreeFingerprint.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.omni.ad.fingerprints
-
-import app.revanced.patcher.fingerprint.MethodFingerprint
-
-internal object IsAdFreeFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef, _ ->
-        methodDef.definingClass.endsWith("PremiumStatus;") && methodDef.name == "isAdFree"
-    }
-)
