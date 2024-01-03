7f48babd903663142a031f49887c0bffa3fd28b0
Casper <42980888+BitesizedLion@users.noreply.github.com>
2024-01-03 03:04:21 +0100

    Delete src/main/kotlin/app/revanced/patches/omni/ad/fingerprints/GetShowFingerprint.kt

--- src/main/kotlin/app/revanced/patches/omni/ad/fingerprints/GetShowFingerprint.kt
+++
@@ -1,7 +0,0 @@
-package app.revanced.patches.omni.ad.fingerprints
-
-import app.revanced.patcher.fingerprint.MethodFingerprint
-
-object LinkFingerprint : MethodFingerprint(
-    strings = listOf("https://omni-ads.omni.news/prod/"),
-)
