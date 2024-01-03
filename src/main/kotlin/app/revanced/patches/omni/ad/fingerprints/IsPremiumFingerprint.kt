0300d4211507f6cb6a0e01db6302266cfdbd34fa
Casper <42980888+BitesizedLion@users.noreply.github.com>
2024-01-03 03:02:58 +0100

    Delete src/main/kotlin/app/revanced/patches/omni/ad/fingerprints/IsPremiumFingerprint.kt

--- src/main/kotlin/app/revanced/patches/omni/ad/fingerprints/IsPremiumFingerprint.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.omni.ad.fingerprints
-
-import app.revanced.patcher.fingerprint.MethodFingerprint
-
-internal object IsPremiumFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef, _ ->
-        methodDef.definingClass.endsWith("PremiumStatus;") && methodDef.name == "isPremium"
-    }
-)
