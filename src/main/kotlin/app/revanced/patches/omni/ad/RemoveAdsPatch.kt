4d189b5d4c33fa27eedc67dddef8f06686963367
Casper <42980888+BitesizedLion@users.noreply.github.com>
2024-01-03 03:04:51 +0100

    fix(asdf): i cba

--- src/main/kotlin/app/revanced/patches/omni/ad/RemoveAdsPatch.kt
+++ src/main/kotlin/app/revanced/patches/omni/ad/RemoveAdsPatch.kt
@@ -6,7 +6,7 @@ import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.omni.ad.fingerprints.GetShowFingerprint
+import app.revanced.patches.omni.ad.fingerprints.LinkFingerprint
 
 @Patch(
     name = "Disable ads",
